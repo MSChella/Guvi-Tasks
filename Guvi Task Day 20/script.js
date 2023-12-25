b91e1d74c64859a6259532a3b5ddb5d061e3668c
MSChella <reachchellapathy.ms@gmail.com>
2023-12-25 18:13:02 +0530

    changes

--- Guvi Task Day 20/script.js
+++ Guvi Task Day 20/script.js
@@ -10,10 +10,29 @@ async function getDogImage(json) {
     await fetch("https://dog.ceo/api/breeds/image/random", { method: "GET" })
         .then((res) => res.json())
         .then((json) => {
-            imgEle.innerHTML = `<img src = '${json.message}'>`
+            imgEle.innerHTML = `<img src = '${json.message}' alt='Dog Image'/>`
             console.log(json)
         })
 
 }
 
 btnEle.onclick = () => getDogImage()
+
+const btnEle2 = document.getElementById("btn_fox")
+const imgFoxEle = document.getElementById("image_fox")
+
+
+
+async function getBearImage(json) {
+    await fetch("https://random-d.uk/api or https://random-d.uk/api/v2", { method: "GET" })
+        .then((res) => res.json())
+        .then((json) => {
+            imgFoxEle.innerHTML = `<img src = '${json.message}' alt='Fox Image'/>`
+            console.log(json)
+
+        })
+        .catch((error) => console.error("this is an error"))
+
+}
+
+btnEle2.onclick = () => getBearImage()
